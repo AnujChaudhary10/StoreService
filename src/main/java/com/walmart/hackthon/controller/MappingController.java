7c3e725facd522cf189aa15959ee1f9b2bb2ac15
AnujChaudhary10 <anuj.Chaudhary@walmart.com>
2021-09-02 01:03:44 +0530

    added method to mapping

--- src/main/java/com/walmart/hackthon/controller/MappingController.java
+++ src/main/java/com/walmart/hackthon/controller/MappingController.java
@@ -1,5 +1,7 @@
 package com.walmart.hackthon.controller;
 
+import java.util.Set;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -7,7 +9,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.walmart.hackthon.common.pojo.Response;
+import com.walmart.hackthon.promotion.pojo.PromotionDetail;
 import com.walmart.hackthon.service.StorePromotionMatcherServiceImpl;
+import com.walmart.hackthon.store.pojo.StoreDetail;
 
 @RestController
 @RequestMapping(value = "/mapping")
@@ -16,8 +20,18 @@ public class MappingController {
 	StorePromotionMatcherServiceImpl impl;
 
 	@GetMapping(value = "/{storeId}/{promotionId}")
-	Response<String> addPromotionToStore(@PathVariable String storeId, @PathVariable String promotionId) {
+	public Response<String> addPromotionToStore(@PathVariable String storeId, @PathVariable String promotionId) {
 		return impl.addPromotionsToStore(storeId, promotionId);
+	}
+	
+	@GetMapping(value = "allPromotion/{storeId}")
+	public Response<Set<PromotionDetail>>  getAllPromotionForStore(@PathVariable String storeId) {
+		return impl.getPromotionsForStoreId(storeId);
+	}
+	
+	@GetMapping(value = "allStore/{promotionId}")
+	public Response<Set<StoreDetail>>   getAllStoreForPromotion(@PathVariable String promotionId) {
+		return impl.getStoreForPromotionId(promotionId);
 	}
 
 }
